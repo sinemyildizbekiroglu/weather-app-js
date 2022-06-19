5f4d17bd57b26d96dcecf864d216f60cf2e487a7
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-06-19 20:58:22 +0200

    Add `querySelector`, `addEventListener`

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ const api = {
   key: "41dbca3720e02fc42427ad1fe2655dd6",
   baseurl: "https://api.openweathermap.org/data/2.5/",
 }
+
+const searchbox = document.querySelector(".search-box")
+searchbox.addEventListener("keypress", setQuery)
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value)
+    console.log(searchbox.value)
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json()
+    })
+    .then(displayResults)
+}
